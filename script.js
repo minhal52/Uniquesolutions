ab1886b548ad6ea255ad6ca95c849cfb08c896fc
minhal52 <minhalhanief@gmail.com>
2024-03-14 14:17:27 +0530

    updated style,script and added sliding automation

--- script.js
+++ script.js
@@ -202,3 +202,26 @@ if (productId) {
 });
 
 
+// automic slide home page
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
